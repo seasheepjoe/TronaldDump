f189c20645ae05aca07ab506d5c747d11e64c1ea
Louis Loiseau-Billon <louis.loiseau-billon@supinternet.fr>
2019-10-31 16:23:04 +0100

    Initial Commit

---
+++ TronaldDump/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  TronaldDump
+//
+//  Created by Louis Loiseau-Billon on 31/10/2019.
+//  Copyright © 2019 Louis Loiseau-Billon. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TronaldDumpTests/TronaldDumpTests.swift
@@ -0,0 +1,34 @@
+//
+//  TronaldDumpTests.swift
+//  TronaldDumpTests
+//
+//  Created by Louis Loiseau-Billon on 31/10/2019.
+//  Copyright © 2019 Louis Loiseau-Billon. All rights reserved.
+//
+
+import XCTest
+@testable import TronaldDump
+
+class TronaldDumpTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
