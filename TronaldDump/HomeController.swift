9383362101afe43ecad52541d7243456d39872f8
Louis Loiseau-Billon <louis.loiseau-billon@supinternet.fr>
2019-11-15 11:44:47 +0100

    Web view working and full content

--- TronaldDump/HomeController.swift
+++ TronaldDump/HomeController.swift
@@ -20,7 +20,7 @@ class HomeController: UIViewController, WKUIDelegate {
         let webConfiguration = WKWebViewConfiguration()
         randomQuoteWV = WKWebView(frame: .zero, configuration: webConfiguration)
         randomQuoteWV.uiDelegate = self
-        self.view.grid(child: randomQuoteWV, x: 0.5, y: 6.5, width: 11, height: 3)
+        self.view.grid(child: randomQuoteWV, x: 0.5, y: 6.5, width: 11, height: 4)
         let api = ApiManager()
         let headerTitle = UILabel()
         headerTitle.text = "Home"
@@ -35,15 +35,14 @@ class HomeController: UIViewController, WKUIDelegate {
             let randomMemeImage = UIImage(data: data)
             api.getQuoteById(quote_id: quoteID, completion: { quote in
                 let data: Quote = quote
-                api.get(url: "https://publish.twitter.com/oembed?url=" + quote.source, completion: { data in
+                api.get(url: "https://publish.twitter.com/oembed?url=" + quote.source + "&align=center", completion: { data in
                     let json = data as! [String:Any]
                     let blockquote = json["html"] as! String
-                    print(blockquote)
                     DispatchQueue.main.async {
                         self.randomQuoteID = quoteID
                         randomMemeView.image = randomMemeImage
                         let quoteUrl = URL(string: "https://publish.twitter.com/oembed?url=" + quote.source)
-                        self.randomQuoteWV.loadHTMLString("<html><body><div style='width: 100vw'" + blockquote + "</body></html>", baseURL: quoteUrl)
+                        self.randomQuoteWV.loadHTMLString("<meta name='viewport' content='initial-scale=1.0'/>" + blockquote, baseURL: quoteUrl)
                     }
                 })
             })
@@ -54,7 +53,7 @@ class HomeController: UIViewController, WKUIDelegate {
         favButton.setTitleColor(UIColor(red: 0, green: 0, blue: 0, alpha: 1), for: .normal)
         favButton.backgroundColor = UIColor(red: 0.55, green: 0.55, blue: 0.57, alpha: 0.2)
         favButton.addTarget(self, action: #selector(addFav), for: .touchUpInside)
-        self.view.grid(child: favButton, x: 0.5, y: 9.5, width: 11, height: 1)
+//        self.view.grid(child: favButton, x: 0.5, y: 9.5, width: 11, height: 1)
     }
 
     @objc func addFav(_ sender: UIButton) {
